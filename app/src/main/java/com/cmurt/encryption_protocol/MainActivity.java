2a75403f033663b6e3d92b277cb2116ea4646376
Connor Murry <CMURTHEEPIC@gmail.com>
2016-01-25 13:07:48 -0600

    Added Date and Time format

--- app/src/main/java/com/cmurt/encryption_protocol/MainActivity.java
+++ app/src/main/java/com/cmurt/encryption_protocol/MainActivity.java
@@ -3,11 +3,21 @@ package com.cmurt.encryption_protocol;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Gets the Date and Time
+        DateFormat dateFormat = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss",Locale.getDefault());
+        Date date = new Date();
+        System.out.println(dateFormat.format(date));
     }
 }
